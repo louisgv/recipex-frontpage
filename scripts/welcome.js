7ef5c0ff15cfb0a8c42ddc643e4a53549fd16cca
Louis Vichy <lgvichy@gmail.com>
2016-04-26 00:11:54 -0700

    New layout

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -30,17 +30,29 @@
 
 		return mt("div.pure-g", [
       mt("div.full-height.pure-u-1-2.app-preview", [
-				mt("div.app-phonecase")
+				mt("div.app-phonecase", [
+					mt("img.img-responsive", {
+						src: './imgs/app.png'
+					})
+				])
 			]),
       mt("div.full-height.pure-u-1-2.feature-request", [
 				mt("div.pure-u-18-24", [
-					mt("div.pure-u-1", [
-						mt("h1", "Cooking is " + (vm.randomScaryWord || "_____") + "?"),
-						mt("hr")
+					mt("div.pure-u-2-5.app-logo", [
+							mt("img.img-logo", {
+							src: './imgs/logo.png'
+						})
 					]),
-					mt("div.pure-u-4-5", [
-						mt("p.text-muted", "Let us help.")
-					])
+					mt("div.pure-u-3-5", [
+						mt("h1", "RecipeX"),
+						mt("h3", "The Rocket Engineer's Cooking Guide"),
+						// mt("hr")
+					]),
+						mt("form.pure-form", [
+								mt("input.half-left-rounded.pure-u-4-5[placeholder='Email me when you launch!'][type='email']"),
+								mt("button.half-right-rounded.pure-button.pure-u-1-5.pure-button-primary[type='submit']", "GO!")
+						]),
+
 				]),
 			]),
     ]);
